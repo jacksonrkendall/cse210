60962e60f39992170bff065591b121b6914a4689
Jackson Kendall <jackson.kendall@icloud.com>
2024-09-21 13:02:46 -0600

    completed c#prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess = 0;
+
+        // generates random number
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 100);
+
+        while (guess != magic)
+        {
+
+            // asks the customer for their guess
+            Console.Write("What is your guess? ");
+            string input2 = Console.ReadLine();
+            guess = int.Parse(input2);
+
+            // checks if the number matches, or if the user needs to guess higher or lower,
+            // then writes out response to user
+            if (guess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
