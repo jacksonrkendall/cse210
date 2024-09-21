bf67269d01e86080477e80aa08f8b694b110410c
Jackson Kendall <jackson.kendall@icloud.com>
2024-09-20 22:23:35 -0600

    Completed C#Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,22 @@
+// Bond, James Bond
+// Input: user is asked for their first and last names
+// Output: terminal outputs user's name in "Bond, James Bond" style
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // asks the user for their first name, assigns it to a variable
+        Console.Write("What is your first name? ");
+        string FirstName = Console.ReadLine();
+        // asks the user for their last name, assigns it to a variable
+        Console.Write("What is your last name? ");
+        string LastName = Console.ReadLine();
+        // prints blank line inbetween arguments for good separation
+        Console.WriteLine();
+        // prints user's name in the Bond, James Bond format
+        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}.");
     }
 }
