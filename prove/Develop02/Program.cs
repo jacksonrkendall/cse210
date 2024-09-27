168a73c91d05e1ee7337f0109e0fb01149bb27d2
Jackson Kendall <jackson.kendall@icloud.com>
2024-09-27 15:05:23 -0600

    working on projects for wk1/2

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        int choice = 0;
+        while (choice != 5)
+        {
+                // offers options, asks user for choice
+                Console.WriteLine("Welcome to the Journal Prrogram.");
+            Console.WriteLine("Make a choice below: \n1: Write\n2: Display\n3: Load\n4: Save\n5: Quit");
+            string response = Console.ReadLine();
+            choice = int.Parse(response);
+            
+            // compares response to different choice levels
+
+            if (choice == 1)
+            {
+                Console.WriteLine("Choice 1");
+            }
+            else if (choice == 2)
+            {
+                Console.WriteLine("Choice 2");
+            }
+            else if (choice == 3)
+            {
+                Console.WriteLine("Choice 3");
+            }
+            else if (choice == 4)
+            {
+                Console.WriteLine("Choice 4");
+            }
+            else if (choice == 5)
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else 
+            {
+                Console.WriteLine("Incorrect entry, please enter {1-5}");
+            }
+        }
     }
 }
